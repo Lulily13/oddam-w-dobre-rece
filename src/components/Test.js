0955509b5b785023d4e22b7560fa61451551c6b2
Lulily13 <bloodyakuma13@yahoo.com>
2022-05-20 16:54:30 +0200

    Created routing structure and added testing component

---
+++ src/components/Test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Test = () => {
+    return (
+        <>
+            <div className="Test">
+                TEST
+            </div>
+        </>
+    )
+}
+
+export default Test;
